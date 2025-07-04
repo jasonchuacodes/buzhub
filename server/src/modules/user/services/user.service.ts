cb095b6aad2ac8ca5555e5a2bebacb43ba96edc8
jasonoffshorly <jason@offshorly.com>
2025-07-05 02:54:46 +0800

    chore: add .env.example.file

--- server/src/modules/user/services/user.service.ts
+++ server/src/modules/user/services/user.service.ts
@@ -1,6 +1,6 @@
 import { prisma } from '~/db/index';
 
-import { Role, User } from '@prisma';
+import { Role } from '@prisma';
 
 const createUser = async (data: { name: string; role: Role; email: string }) => {
     return prisma.user.create({ data });
